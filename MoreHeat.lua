81027a9461b19d89a034fda0960439d1d6b86e5e
paradigmsort <paradigmsort+hades@gmail.com>
2020-12-01 11:24:48 -0800

    wip

--- MoreHeat.lua
+++ MoreHeat.lua
@@ -7,13 +7,57 @@ ModUtil.LoadOnce(function()
   MetaUpgradeData.TrapDamageShrineUpgrade.MaxInvestment = 2
   MetaUpgradeData.TrapDamageShrineUpgrade.HelpTextTable = nil
   MetaUpgradeData.EnemySpeedShrineUpgrade.MaxInvestment = 4
+  MetaUpgradeData.ReducedLootChoicesShrineUpgrade.CostTable = { 2, 3, 4 }
   MetaUpgradeData.EnemyShieldShrineUpgrade.CostTable = { 1, 1, 1, 1 }
   MetaUpgradeData.EnemyCountShrineUpgrade.CostTable = { 1, 1, 1, 1, 1, 1 }
   MetaUpgradeData.EnemyEliteShrineUpgrade.CostTable = { 2, 3, 3, 3 }
-  MetaUpgradeData.BiomeSpeedShrineUpgrade.CostTable = { 1, 2, 3, 4, 5 }
+  MetaUpgradeData.BiomeSpeedShrineUpgrade.CostTable = { 1, 2, 3, 4 }
   MetaUpgradeData.BiomeSpeedShrineUpgrade.HelpTextTable[4] = "BiomeSpeedShrineUpgrade_4"
-  MetaUpgradeData.BiomeSpeedShrineUpgrade.HelpTextTable[5] = "BiomeSpeedShrineUpgrade_5"
   BiomeTimeLimits.Timers[4] = { 0, 180, 180, 180, 180 }
-  BiomeTimeLimits.Timers[5] = { 0, 60, 60, 60, 60 }
   MetaUpgradeData.NoInvulnerabilityShrineUpgrade.GameStateRequirements = nil
 end)
+
+function MoreHeat.CloneFunction(func, wrapper)
+  local newEnv = {}
+  setmetatable(newEnv, {__index = _G})
+  local newFunc = load(string.dump(func), nil, "b", newEnv)
+  return wrapper(newEnv, newFunc)
+end
+
+function MoreHeat.WrapWithin(caller, callee, wrapper)
+  local newEnv = {}
+  setmetatable(newEnv, {__index = _G})
+  local oldCallee = newEnv[callee]
+  newEnv[callee] = function(...)
+    return wrapper(oldCallee, ...)
+  end
+  ModUtil.BaseOverride(caller, load(string.dump(_G[caller]), nil, "b", newEnv))
+end
+
+MoreHeat.CreateBoonLootButtons = MoreHeat.CloneFunction(CreateBoonLootButtons, function(env, func)
+  local originalIsMetaUpgradeSelected = env.IsMetaUpgradeSelected
+  env.IsMetaUpgradeSelected = function(name)
+    local components = ScreenAnchors.ChoiceScreen.Components
+    if name == "RerollPanelMetaUpgrade" and CalcNumLootChoices() == 0 then
+      components["RerollPanel"] = CreateScreenComponent({ Name = "ButtonClose", Scale = 0.7, Group = "Combat_Menu_TraitTray" })
+      Attach({ Id = components["RerollPanel"].Id, DestinationId = components.ShopBackground.Id, OffsetX = 0, OffsetY = 410 })
+      components["RerollPanel"].OnPressedFunctionName = "CancelUpgrade"
+      return false
+    else
+      return originalIsMetaUpgradeSelected(name)
+    end
+  end
+  return func
+end)
+
+ModUtil.BaseOverride("CreateBoonLootButtons", function( lootData, reroll )
+  return MoreHeat.CreateBoonLootButtons(lootData, reroll)
+end)
+
+function CancelUpgrade(screen, button)
+  CloseUpgradeChoiceScreen( screen, button )
+  wait( 0.2, RoomThreadName )
+  if CheckRoomExitsReady( CurrentRun.CurrentRoom ) then
+    UnlockRoomExits( CurrentRun, CurrentRun,CurrentRoom )
+  end
+end
